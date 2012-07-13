ad5fb7cf2948549334da2ed0a8db80a78abc259b
Laércio de Sousa <lbsousajr@gmail.com>
2012-07-13 11:09:03 -0300

    Update pai-install-data.sh and pai-install-doc.in
    - These scripts are used by Makefile to install SEI data and documentation
      through the new functions included in file linux/share/functions.in.

--- pai-install-data.sh
+++ pai-install-data.sh
@@ -12,11 +12,13 @@ case "`lsb_release -sc`" in
     ;;
 esac
 
-download_dir=${download_dir:-`mktemp -d ${MKTEMP_OPTS} sei-download-XXXXXX`}
-unpack_dir=${unpack_dir:-`mktemp -d ${MKTEMP_OPTS} sei-unpack-XXXXXX`}
-orig_dir=${unpack_dir}/usr/share/pte-pmmc/menu_sei
+download_dir=${download_dir:-`mktemp -d ${MKTEMP_OPTS} pai-download-XXXXXX`}
+unpack_dir=${unpack_dir:-`mktemp -d ${MKTEMP_OPTS} pai-unpack-XXXXXX`}
+orig_dir=${unpack_dir}/usr/share/PTE-PMMC/pai
 dest_dir=${1}
 
-sei_download ${download_dir}
-sei_unpack ${download_dir} ${unpack_dir}
-sei_install_data ${orig_dir} ${dest_dir} && rm -rf ${download_dir} ${unpack_dir}
+pai_download ${download_dir}
+pai_unpack ${download_dir} ${unpack_dir}
+pai_install_data ${orig_dir} ${dest_dir}
+pai_patch_data ${dest_dir}
+rm -rf ${download_dir} ${unpack_dir}
